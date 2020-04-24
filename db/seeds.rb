833aa09ded9ff922a4e4f73b44882b3c613c604e
altan <altanmk93@gmail.com>
2020-04-24 18:29:57 +0200

    created illness model and table with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-question_one = Question.find(10) # fever
-question_two = Question.find(11)
-question_three = Question.find(12)
-question_four = Question.find(13)
-question_five = Question.find(14)
-question_six = Question.find(15)
-question_seven = Question.find(16)
-question_eight = Question.find(17)
+question_one = Question.create(text: "Hast du Fieber?")
+question_two = Question.create(text: "Hast du Gliederschmerzen?")
+question_three = Question.create(text: "Hast du Halsschmerzen?")
+question_four = Question.create(text: "Hast du Husten?")
+question_five = Question.create(text: "Hast du Kopfschmerzen?")
+question_six = Question.create(text: "Fühlst du dich müde?")
+question_seven = Question.create(text: "Musst du niesen?")
+question_eight = Question.create(text: "Hast du Schnupfen?")
 
 Answer.create(option: "häufig", category: "fever", question: question_one)
 Answer.create(option: "selten", category: "fever", question: question_one)
@@ -21,7 +21,37 @@ Answer.create(option: "häufig", category: "head", question: question_five)
 Answer.create(option: "manchmal", category: "fatigue", question: question_six)
 Answer.create(option: "häufig", category: "fatigue", question: question_six)
 Answer.create(option: "häufig", category: "sneeze", question: question_seven)
+Answer.create(option: "nein", category: "sneeze", question: question_seven)
 Answer.create(option: "selten", category: "sniff", question: question_eight)
 Answer.create(option: "häufig", category: "sniff", question: question_eight)
 Answer.create(option: "manchmal", category: "sniff", question: question_eight)
+
+
+
+answer_fever = Answer.find_by(:category => 'fever', :option => 'häufig' ).id
+answer_fever_ = Answer.find_by(:category => 'fever', :option => 'selten' ).id
+answer_limbs = Answer.find_by(:category => 'limbs', :option => 'manchmal' ).id
+answer_limbs_ = Answer.find_by(:category => 'limbs', :option => 'häufig' ).id
+answer_neck = Answer.find_by(:category => 'neck', :option => 'manchmal' ).id
+answer_neck_ = Answer.find_by(:category => 'neck', :option => 'häufig' ).id
+answer_cough = Answer.find_by(:category => 'cough', :option => 'häufig (trocken)' ).id
+answer_cough_ = Answer.find_by(:category => 'cough', :option => 'wenig' ).id
+answer_head = Answer.find_by(:category => 'head', :option => 'manchmal' ).id
+answer_head_ = Answer.find_by(:category => 'head', :option => 'selten' ).id
+answer_head__ = Answer.find_by(:category => 'head', :option => 'häufig' ).id
+answer_fatigue = Answer.find_by(:category => 'fatigue', :option => 'manchmal' ).id
+answer_fatigue_ = Answer.find_by(:category => 'fatigue', :option => 'häufig' ).id
+answer_sneeze = Answer.find_by(:category => 'sneeze', :option => 'nein' ).id
+answer_sneeze_ = Answer.find_by(:category => 'sneeze', :option => 'häufig' ).id
+answer_sniff = Answer.find_by(:category => 'sniff', :option => 'selten' ).id
+answer_sniff_ = Answer.find_by(:category => 'sniff', :option => 'häufig' ).id
+answer_sniff__ = Answer.find_by(:category => 'sniff', :option => 'manchmal' ).id
+
+
+Illness.create(name: "Coronavirus", answer_ids: [answer_fever, answer_limbs, answer_neck, answer_cough, answer_head,
+                                                 answer_fatigue, answer_sneeze, answer_sniff ]) # coronavirus
+Illness.create(name: "Erkältung", answer_ids: [answer_fever_, answer_limbs_, answer_neck_, answer_cough_,
+                                                 answer_head_, answer_fatigue, answer_sneeze_, answer_sniff_]) # cold
+Illness.create(name: "Grippe", answer_ids: [answer_fever, answer_limbs_, answer_neck, answer_cough, answer_head__,
+                                                 answer_fatigue_, answer_sneeze, answer_sniff__ ]) # flu
 
